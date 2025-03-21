a518db01ec07808e6de5651ffe565280c392a6e0
MonishaDR-25 <monishadr25@gmail.com>
2025-03-21 15:34:28 +0530

    pushing all the code to git

---
+++ Boxcolor.java
@@ -0,0 +1,110 @@
+class Boxcolor{
+	public static void main(String[] args){
+		String color1="red";
+		String color2="blue";
+		String color3="green";
+		String color4="yellow";
+		String[] colors = {
+            "Red", "Blue", "Green", "Yellow", "Orange", "Purple", "Pink", "Brown", "Black", "White",
+            "Gray", "Cyan", "Magenta", "Teal", "Lime", "Maroon", "Navy", "Olive", "Gold", "Silver",
+            "Beige", "Turquoise", "Violet", "Indigo", "Peach", "Coral", "Lavender", "Amber", "Mint", "Azure"
+        };
+	    System.out.println("Element at index 0: " + colors[0]);
+        System.out.println("Element at index 1: " + colors[1]);
+        System.out.println("Element at index 2: " + colors[2]);
+        System.out.println("Element at index 3: " + colors[3]);
+        System.out.println("Element at index 4: " + colors[4]);
+        System.out.println("Element at index 5: " + colors[5]);
+        System.out.println("Element at index 6: " + colors[6]);
+        System.out.println("Element at index 7: " + colors[7]);
+        System.out.println("Element at index 8: " + colors[8]);
+        System.out.println("Element at index 9: " + colors[9]);
+        System.out.println("Element at index 10: " + colors[10]);
+        System.out.println("Element at index 11: " + colors[11]);
+        System.out.println("Element at index 12: " + colors[12]);
+        System.out.println("Element at index 13: " + colors[13]);
+        System.out.println("Element at index 14: " + colors[14]);
+        System.out.println("Element at index 15: " + colors[15]);
+        System.out.println("Element at index 16: " + colors[16]);
+        System.out.println("Element at index 17: " + colors[17]);
+        System.out.println("Element at index 18: " + colors[18]);
+        System.out.println("Element at index 19: " + colors[19]);
+        System.out.println("Element at index 20: " + colors[20]);
+        System.out.println("Element at index 21: " + colors[21]);
+        System.out.println("Element at index 22: " + colors[22]);
+        System.out.println("Element at index 23: " + colors[23]);
+        System.out.println("Element at index 24: " + colors[24]);
+        System.out.println("Element at index 25: " + colors[25]);
+        System.out.println("Element at index 26: " + colors[26]);
+        System.out.println("Element at index 27: " + colors[27]);
+        System.out.println("Element at index 28: " + colors[28]);
+        System.out.println("Element at index 29: " + colors[29]);
+		
+		 // Step 4: Print updated elements 
+		colors[0] = "Crimson";
+        colors[1] = "Sky Blue";
+        colors[2] = "Forest Green";
+        colors[3] = "Mustard";
+        colors[4] = "Tangerine";
+        colors[5] = "Plum";
+        colors[6] = "Rose";
+        colors[7] = "Chestnut";
+        colors[8] = "Charcoal";
+        colors[9] = "Ivory";
+        colors[10] = "Ash";
+        colors[11] = "Aqua";
+        colors[12] = "Fuchsia";
+        colors[13] = "Sea Green";
+        colors[14] = "Chartreuse";
+        colors[15] = "Burgundy";
+        colors[16] = "Royal Blue";
+        colors[17] = "Khaki";
+        colors[18] = "Bronze";
+        colors[19] = "Platinum";
+        colors[20] = "Sand";
+        colors[21] = "Cobalt";
+        colors[22] = "Lilac";
+        colors[23] = "Denim";
+        colors[24] = "Apricot";
+        colors[25] = "Salmon";
+        colors[26] = "Periwinkle";
+        colors[27] = "Honey";
+        colors[28] = "Spearmint";
+        colors[29] = "Sapphire";
+
+        // Step 4: Print updated elements 
+        System.out.println("Element at index 0: " + colors[0]);
+        System.out.println("Element at index 1: " + colors[1]);
+        System.out.println("Element at index 2: " + colors[2]);
+        System.out.println("Element at index 3: " + colors[3]);
+        System.out.println("Element at index 4: " + colors[4]);
+        System.out.println("Element at index 5: " + colors[5]);
+        System.out.println("Element at index 6: " + colors[6]);
+        System.out.println("Element at index 7: " + colors[7]);
+        System.out.println("Element at index 8: " + colors[8]);
+        System.out.println("Element at index 9: " + colors[9]);
+        System.out.println("Element at index 10: " + colors[10]);
+        System.out.println("Element at index 11: " + colors[11]);
+        System.out.println("Element at index 12: " + colors[12]);
+        System.out.println("Element at index 13: " + colors[13]);
+        System.out.println("Element at index 14: " + colors[14]);
+        System.out.println("Element at index 15: " + colors[15]);
+        System.out.println("Element at index 16: " + colors[16]);
+        System.out.println("Element at index 17: " + colors[17]);
+        System.out.println("Element at index 18: " + colors[18]);
+        System.out.println("Element at index 19: " + colors[19]);
+        System.out.println("Element at index 20: " + colors[20]);
+        System.out.println("Element at index 21: " + colors[21]);
+        System.out.println("Element at index 22: " + colors[22]);
+        System.out.println("Element at index 23: " + colors[23]);
+        System.out.println("Element at index 24: " + colors[24]);
+        System.out.println("Element at index 25: " + colors[25]);
+        System.out.println("Element at index 26: " + colors[26]);
+        System.out.println("Element at index 27: " + colors[27]);
+        System.out.println("Element at index 28: " + colors[28]);
+        System.out.println("Element at index 29: " + colors[29]);
+
+		
+        		
+	}
+}
